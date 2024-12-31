70672881651203697028236220107e4e340ce4cf
arehman-dev <az.rehman215@gmail.com>
2024-12-31 10:36:24 +0500

    Comment out event listener code in weather extension scripts
    v0.1

--- javascript/project4-weather-extension/background.js
+++ javascript/project4-weather-extension/background.js
@@ -1,38 +1,38 @@
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-          const activeTab = tabs[0];
-          // Send a message to the active tab
-          chrome.tabs.sendMessage(activeTab.id, { message: "Alert! Alert! Alert!" });
-          console.log("Extension clicked!");
-      }
-  });
-});
+// chrome.action.onClicked.addListener(() => {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       if (tabs.length > 0) {
+//           const activeTab = tabs[0];
+//           // Send a message to the active tab
+//           chrome.tabs.sendMessage(activeTab.id, { message: "Alert! Alert! Alert!" });
+//           console.log("Extension clicked!");
+//       }
+//   });
+// });
+
+// // // chrome.action.onClicked.addListener((tab) => {
+// // //   //all tabs query
+// // //   chrome.tabs.query({}, (tabs) => {
+// // //     tabs.forEach((t) => {
+// // //       //loging
+// // //       chrome.scripting.executeScript({
+// // //         target: { tabId: t.id },
+// // //         func: logmessager
+
+// // //       });
+// // //     });
+      
+// // //     });
+// // //   });
+
+// // // function logmessager() {
+// // //   console.log("Extension clicked!")
+// // // }
 
 // // chrome.action.onClicked.addListener((tab) => {
-// //   //all tabs query
-// //   chrome.tabs.query({}, (tabs) => {
-// //     tabs.forEach((t) => {
-// //       //loging
-// //       chrome.scripting.executeScript({
-// //         target: { tabId: t.id },
-// //         func: logmessager
 
-// //       });
-// //     });
-      
-// //     });
-// //   });
-
-// // function logmessager() {
-// //   console.log("Extension clicked!")
-// // }
-
-// chrome.action.onClicked.addListener((tab) => {
-
-//   if (tab.id) {
-//     // Send a message to active tab
-//     chrome.tabs.sendMessage(tab.id, { message: "Alert! Alert! Alert!" });
-//     console.log("Extension clicked!");  
-//   }
-// });
+// //   if (tab.id) {
+// //     // Send a message to active tab
+// //     chrome.tabs.sendMessage(tab.id, { message: "Alert! Alert! Alert!" });
+// //     console.log("Extension clicked!");  
+// //   }
+// // });
--- javascript/project4-weather-extension/content.js
+++ javascript/project4-weather-extension/content.js
@@ -1,17 +1,17 @@
-// Listen for messages
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.message === "Alert! Alert! Alert!") {
-    console.log("Message received from extension: ", request.message);
-    sendResponse({ status: "Message logged!" });
-  }
-});
-
-
-// // For active tab consoles
-// // // Listen messages
+// // Listen for messages
 // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     if (request.message === "Alert! Alert! Alert!") {
-//       console.log("Message received from extension: ", request.message);
-//       sendResponse({ status: "Message logged!" });
-//     }
-//   });
+//   if (request.message === "Alert! Alert! Alert!") {
+//     console.log("Message received from extension: ", request.message);
+//     sendResponse({ status: "Message logged!" });
+//   }
+// });
+
+
+// // // For active tab consoles
+// // // // Listen messages
+// // chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+// //     if (request.message === "Alert! Alert! Alert!") {
+// //       console.log("Message received from extension: ", request.message);
+// //       sendResponse({ status: "Message logged!" });
+// //     }
+// //   });
