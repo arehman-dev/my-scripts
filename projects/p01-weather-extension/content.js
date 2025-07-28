9b095afc4642a0f22108e5888e1d47307e9c9448
arehman-dev <az.rehman215@gmail.com>
2025-07-28 14:13:24 +0500

    working on v0.5

--- projects/p01-weather-extension/content.js
+++ projects/p01-weather-extension/content.js
@@ -1,12 +1,12 @@
-// Log a simple message when the content script is injected 
-// using manifest.
-console.log("Weather Extension content script is running.");
+// // Log a simple message when the content script is injected 
+// // using manifest.
+// console.log("Weather Extension content script is running.");
 
-// Listen for messages from the popup script 
-// using popup button
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "Log this message") {
-        console.log("message from popup button");
-        sendResponse({ status: "Done! finally" });
-    }
-});
+// // Listen for messages from the popup script 
+// // using popup button
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (request.message === "Log this message") {
+//         console.log("message from popup button");
+//         sendResponse({ status: "Done! finally" });
+//     }
+// });
